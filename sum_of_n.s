664757b58e7c6e60b3823a8409431d342a7f8731
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-03-03 11:04:13 +0530

    Create sum_of_n.s

---
+++ sum_of_n.s
@@ -0,0 +1,17 @@
+  AREA mul, CODE, READONLY
+  ENTRY
+  EXPORT __main
+__main
+  MOV R0, #1
+
+loop
+  ADD R1, R0
+  ADD R0, #1
+  CMP R0, #11
+  BNE loop
+  LDR R2, =Result;
+  STR R0, [R2];
+STOP B STOP
+AREA Res, DATA , READWRITE
+Result DCD 0x00;
+END
